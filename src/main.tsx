fd87c984c789bc834982b1cb7c789b76bc97ee0d
dongjinoh-dev <odj9517@gmail.com>
2023-04-11 17:37:20 +0900

    성공

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-  <div className="quiz-container">
     <Quiz />
-  </div>
 </React.StrictMode>,
 )
